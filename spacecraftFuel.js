ad635579d6afd089104c550a1f83c34a310e7f2b
Deepika <bhuvanadeepika017@gmail.com>
2020-04-09 22:40:49 +0530

    added question answer input for day 1

---
+++ spacecraftFuel.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+const calculateFuel = function(input, initialFuel) {
+  let totalFuel = initialFuel;
+  const fuel = Math.floor(input / 3) - 2;
+  if (fuel <= 0) {
+    return totalFuel;
+  }
+  totalFuel += fuel;
+  return calculateFuel(fuel, totalFuel);
+}
+
+const getTotalFuel = function(inputs) {
+  let totalFuel = 0;
+  for (let i = 0; i < inputs.length; i++) {
+    totalFuel += calculateFuel(inputs[i], 0)
+  }
+  return totalFuel;
+}
+
+const main = function() {
+  const inputs = fs.readFileSync('./inputs/inputMasses.txt', 'utf8').split('\n');
+  const totalFuel = getTotalFuel(inputs)
+  console.log(totalFuel);
+}
+
+main();
+
