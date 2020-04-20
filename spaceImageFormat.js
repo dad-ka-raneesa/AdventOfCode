4c9a566803640a0842b7bc8571c59517ba664328
Deepika <bhuvanadeepika017@gmail.com>
2020-04-20 22:19:22 +0530

    modified the getAllCount function spaceImageFormat

--- spaceImageFormat.js
+++ spaceImageFormat.js
@@ -1,28 +1,24 @@
 //part1 answer 2048
 const fs = require('fs');
 
-const getCounts = function(counts, rows) {
-  const countOf0 = rows.filter(x => x == 0).length;
+const getCounts = function(counts, layer) {
+  const countOf0 = layer.filter(x => x == 0).length;
   if (countOf0 < counts.count0) {
     counts.count0 = countOf0;
-    counts.count1 = rows.filter(x => x == 1).length;
-    counts.count2 = rows.filter(x => x == 2).length;
+    counts.count1 = layer.filter(x => x == 1).length;
+    counts.count2 = layer.filter(x => x == 2).length;
   }
   return counts;
 }
 
 const getMulOfCounts = function(pixels, width, height) {
   let counts = { count0: Infinity, count1: 0, count2: 0 };
-  let index = 0;
-  let lastIndex = 0
-  while (index < pixels.length) {
-    let layer = [];
-    for (let j = 0; j < height; j++) {
-      layer.push(pixels.slice(lastIndex, lastIndex + width));
-      lastIndex += width;
-    }
-    counts = getCounts(counts, layer.flat());
-    index += height * width;
+  let currentIndex = 0;
+  let lastIndex = 0;
+  while (lastIndex < pixels.length) {
+    lastIndex += width * height;
+    counts = getCounts(counts, pixels.slice(currentIndex, lastIndex));
+    currentIndex = lastIndex;
   }
   return (counts.count1 * counts.count2);
 }
