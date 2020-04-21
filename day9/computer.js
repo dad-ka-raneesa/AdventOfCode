3e8c0ecb57fab0f4b23449af6eb5eee36c31906b
Deepika <bhuvanadeepika017@gmail.com>
2020-04-21 23:49:15 +0530

    added day9 part1 question,input and answer and added computer class

---
+++ day9/computer.js
@@ -0,0 +1,89 @@
+class Computer {
+  constructor(data, inputValue) {
+    this.data = data.slice();
+    this.inputValue = inputValue;
+    this.pointer = 0;
+    this.relativeBase = 0;
+    this.count = 0;
+    this.outputs = [];
+    this.isHalted = false;
+  }
+  getParams() {
+    const fullOpcode = this.data[this.pointer].toString().padStart(5, 0);
+    let [param3mode, param2mode, param1mode, ...opcode] = fullOpcode.split('');
+    opcode = opcode.join('');
+    let operand1 = this.data[this.pointer + 1];
+    let operand2 = this.data[this.pointer + 2];
+    let outputPath = this.data[this.pointer + 3];
+    if (param1mode == 0) operand1 = this.data[operand1];
+    if (param2mode == 0) operand2 = this.data[operand2];
+    if (param1mode == 2) {
+      if (['03', '04'].includes(opcode))
+        operand1 = operand1 + this.relativeBase;
+      else operand1 = this.data[operand1 + this.relativeBase];
+    }
+    if (param2mode == 2) {
+      if (['03', '04'].includes(opcode))
+        operand2 = operand2 + this.relativeBase;
+      else operand2 = this.data[operand2 + this.relativeBase];
+    }
+    if (param3mode == 2) {
+      outputPath = outputPath + this.relativeBase;
+    }
+    return { opcode, operand1, operand2, outputPath };
+  }
+
+  run(input2) {
+    while (this.pointer < this.data.length) {
+      const { opcode, operand1, operand2, outputPath } = this.getParams();
+      if (opcode == '99') {
+        this.isHalted = true;
+        break;
+      }
+      if (opcode == '01') {
+        this.data[outputPath] = +operand1 + +operand2;
+        this.pointer += 4;
+      }
+      if (opcode == '02') {
+        this.data[outputPath] = +operand1 * +operand2;
+        this.pointer += 4;
+      }
+      if (opcode == '03') {
+        if (this.count == 0) {
+          this.data[operand1] = this.inputValue;
+        } else {
+          this.data[operand1] = input2;
+        }
+        this.count++;
+        this.pointer += 2;
+      }
+      if (opcode == '04') {
+        this.pointer += 2;
+        const output = this.data[operand1] ? this.data[operand1] : operand1;
+        this.outputs.push(output);
+        break;
+      }
+      if (opcode == '05') {
+        this.pointer = operand1 != 0 ? operand2 : this.pointer + 3;
+      }
+      if (opcode == '06') {
+        this.pointer = operand1 == 0 ? operand2 : this.pointer + 3;
+      }
+      if (opcode == '07') {
+        this.data[outputPath] = operand1 < operand2 ? 1 : 0;
+        this.pointer += 4;
+      }
+      if (opcode == '08') {
+        this.data[outputPath] = operand1 == operand2 ? 1 : 0;
+        this.pointer += 4;
+      }
+      if (opcode == '09') {
+        this.relativeBase += operand1;
+        this.pointer += 2;
+      }
+    }
+    return this.outputs;
+  }
+}
+
+module.exports = { Computer };
---
+++ day9/partOne.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const { Computer } = require('./computer');
+
+const main = function() {
+  let inputs = fs.readFileSync('./day9/BOOST.txt', 'utf8').split(',');
+  inputs = inputs.map(num => +num);
+  const computer = new Computer(inputs, 1);
+  const outputs = computer.run(1);
+  console.log(outputs.shift());
+}
+
+main();
