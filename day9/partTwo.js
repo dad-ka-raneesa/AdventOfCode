b55a83312c061a3c12836463cea7e78af8e71183
Deepika <bhuvanadeepika017@gmail.com>
2020-04-21 23:52:32 +0530

    added day9 part2 question,input and answer and added computer class

--- day9/partTwo.js
+++ day9/partTwo.js
@@ -1,12 +1,12 @@
-//answer 3765554916
+//answer 76642
 const fs = require('fs');
 const { Computer } = require('./computer');
 
 const main = function() {
   let inputs = fs.readFileSync('./day9/BOOST.txt', 'utf8').split(',');
   inputs = inputs.map(num => +num);
-  const computer = new Computer(inputs, 1);
-  const outputs = computer.run(1);
+  const computer = new Computer(inputs, 2);
+  const outputs = computer.run(2);
   console.log(outputs.shift());
 }
 
