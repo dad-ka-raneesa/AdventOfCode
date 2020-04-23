73779b9325a220e49bc0e92d43867fa588608f0b
Deepika <bhuvanadeepika017@gmail.com>
2020-04-23 12:45:47 +0530

    modified asteroids.js

--- day10/asteroids.js
+++ day10/asteroids.js
@@ -2,9 +2,11 @@ const getMax = (bestStation, asteroid) => bestStation.detectedAsteroidCount > as
 
 const getAngle = (pos1, pos2) => Math.atan2(pos1.x - pos2.x, pos1.y - pos2.y);
 
+const getRemainingAsteroids = (asteroids, station) => asteroids.filter(pos => station.x != pos.x || station.y != pos.y);
+
 const getAsteroidPositionsInfo = function(positions) {
   return positions.map(station => {
-    const remainingAsteroid = positions.filter(pos => station.x != pos.x || station.y != pos.y);
+    const remainingAsteroid = getRemainingAsteroids(positions, station);
     const angles = remainingAsteroid.map(asteroid => getAngle(station, asteroid));
     const detectedAsteroids = [...new Set(angles)];
     return { pos: station, detectedAsteroidCount: detectedAsteroids.length };
@@ -12,10 +14,9 @@ const getAsteroidPositionsInfo = function(positions) {
 }
 
 const getAsteroidPosition = function(allCoordinates, locations, colId) {
-  return locations.reduce((allCoordinates, location, rowId) => {
-    location === '#' && allCoordinates.push({ x: rowId, y: colId });
-    return allCoordinates;
-  }, allCoordinates)
+  locations.forEach((location, rowId) => location === '#' && allCoordinates.push({ x: rowId, y: colId }));
+  return allCoordinates;
 }
+
 
 module.exports = { getAsteroidPosition, getAsteroidPositionsInfo, getAngle, getMax };
